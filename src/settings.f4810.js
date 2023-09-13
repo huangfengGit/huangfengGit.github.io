61cbb8d292f318ae864ab4966e0da2665afa0722
rubix <41936280@qq.com>
2023-09-13 17:26:27 +0800

    modify ： creator项目上传

---
+++ src/settings.f4810.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/New Scene.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",main:"1e3f5"}};
