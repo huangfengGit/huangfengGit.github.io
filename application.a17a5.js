684cfbea355edd882a66def93895daabd9b050f8
rubix <41936280@qq.com>
2023-12-13 19:39:16 +0800

    update

--- application.a17a5.js
+++ application.a17a5.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.ba400.json';
+          this.settingsPath = 'src/settings.0a962.json';
           this.showFPS = false;
         }
 
