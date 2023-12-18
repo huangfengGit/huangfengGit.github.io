be343a2f3eb59804a1b32497ac5393a912c562e0
rubix <41936280@qq.com>
2023-12-18 15:12:25 +0800

    modify : 优化bg，loading

---
+++ index.73165.js
@@ -0,0 +1,25 @@
+System.register(["./application.b6ba8.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationB6ba8Js) {
+      Application = _applicationB6ba8Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
